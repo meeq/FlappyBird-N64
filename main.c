f944cb2b5dc3cec18229c5fad25426509e9a133e
Christopher Bonhage <me@christopherbonhage.com>
2014-03-12 21:42:39 -0700

    Add trivial audio test code

--- main.c
+++ main.c
@@ -15,6 +15,19 @@ sprite_t *read_dfs_sprite(char *file)
     return sprite;
 }
 
+raw_pcm_t read_dfs_raw_pcm(char *file)
+{
+    int fp = dfs_open( file );
+    int size = dfs_size( fp );
+    s16 *data = malloc( size );
+    dfs_read( data, 1, size, fp );
+    dfs_close( fp );
+    raw_pcm_t sound;
+    sound.numSamples = size / sizeof(u16);
+    sound.sampleData = data;
+    return sound;
+}
+
 int main(void)
 {
     /* enable interrupts (on the CPU) */
@@ -30,12 +43,21 @@ int main(void)
     timer_init();
     dfs_init( DFS_DEFAULT_LOCATION );
 
+    /* Initialize audio */
+    audio_init( FREQUENCY_44KHZ, 1 );
+    int audioBuffSize = audio_get_buffer_length() * STEREO_PCM_SAMPLE_SIZE;
+    s16* audioBuff = malloc( audioBuffSize );
+    audio_write_silence();
+
+    raw_pcm_t sound = read_dfs_raw_pcm("/sfx/test.raw");
+    int samplesBuffered = 0;
+
     /* Set up main loop */
 
     u32 bgColor = graphics_make_color( 0xDD, 0xEE, 0xFF, 0xFF );
     u32 fgColor = graphics_make_color( 0x00, 0x00, 0x00, 0x00 );
 
-    char *testString = "Trivial font/color test";
+    char *testString = "Trivial font/color/audio test";
 
     /* Run the main loop */
     while(1)
@@ -55,5 +77,23 @@ int main(void)
 
         /* Force backbuffer flip */
         display_show(disp);
+
+        if (audio_can_write())
+        {
+             sprintf(testString, "Samples %i/%i", samplesBuffered, sound.numSamples);
+            if (samplesBuffered < sound.numSamples)
+            {
+                s16 sample = 0;
+                int writeSamples = samplesBuffered + (audioBuffSize / STEREO_PCM_SAMPLE_SIZE);
+                for (int i = 0; samplesBuffered < writeSamples; samplesBuffered += 1, i += 2)
+                {
+                    sample = (samplesBuffered < sound.numSamples) ? sound.sampleData[samplesBuffered] : 0;
+                    audioBuff[i] = audioBuff[i + 1] = sample;
+                }
+                audio_write(audioBuff);
+            } else {
+                audio_write_silence();
+            }
+        }
     }
 }
--- system.h
+++ system.h
@@ -5,8 +5,20 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef uint64_t u64;
+typedef signed short s16;
 
 /* Video definitions */
 
 #define BUFFERING_DOUBLE 2
 #define BUFFERING_TRIPLE 3
+
+/* Audio definitions */
+
+#define FREQUENCY_44KHZ 44100
+#define STEREO_PCM_SAMPLE_SIZE (2 * sizeof(s16))
+
+struct raw_pcm {
+  int numSamples;
+  s16 *sampleData;
+};
+typedef struct raw_pcm raw_pcm_t;
