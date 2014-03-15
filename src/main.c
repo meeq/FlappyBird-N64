d0b9e2d2a2fecf8fa9b2be4c1b2ae26a23fb6dde
Christopher Bonhage <me@christopherbonhage.com>
2014-03-15 14:52:50 -0700

    Draw repeating sprites using RDP

--- src/main.c
+++ src/main.c
@@ -37,8 +37,35 @@ int main(void)
 
     /* Set up main loop */
 
-    u32 bg_color = graphics_make_color( 0xDD, 0xEE, 0xFF, 0xFF );
-    u32 fg_color = graphics_make_color( 0x00, 0x00, 0x00, 0x00 );
+    u32 sky_fill_color = graphics_make_color( 0x4E, 0xC0, 0xCA, 0xFF );
+
+    sprite_t *cloud_sprite = read_dfs_sprite("/gfx/bg-clouds-day.sprite");
+    s16 cloud_repeat_y = 133;
+    s16 cloud_repeat_h = 11;
+
+    u32 cloud_fill_color = graphics_make_color( 0xE9, 0xFC, 0xD9, 0xFF );
+    s16 cloud_fill_y = 141;
+    s16 cloud_fill_h = 22;
+
+    sprite_t *city_sprite = read_dfs_sprite("/gfx/bg-city-day.sprite");
+    s16 city_repeat_y = 146;
+    s16 city_repeat_h = 18;
+
+    sprite_t *hill_sprite = read_dfs_sprite("/gfx/bg-hill-day.sprite");
+    s16 hill_repeat_y = 160;
+    s16 hill_repeat_h = 9;
+
+    u32 hill_fill_color = graphics_make_color( 0x5E, 0xE2, 0x70, 0xFF );
+    s16 hill_fill_y = 169;
+    s16 hill_fill_h = 21;
+
+    sprite_t *ground_sprite = read_dfs_sprite("/gfx/ground.sprite");
+    s16 ground_repeat_y = 190;
+    s16 ground_repeat_h = 11;
+
+    u32 ground_fill_color = graphics_make_color( 0xDF, 0xD8, 0x93, 0xFF );
+    s16 ground_fill_y = 200;
+    s16 ground_fill_h = 40;
 
     /* Run the main loop */
     while(1)
@@ -50,15 +77,79 @@ int main(void)
         /* Grab a render buffer */
         while( !(disp = display_lock()) );
 
-        /*Fill the screen */
-        graphics_fill_screen( disp, bg_color );
+        /* Draw the sky fill */
+        graphics_fill_screen( disp, sky_fill_color );
 
-        /* Set the text output color */
-        graphics_set_color( fg_color, bg_color );
+        /* Draw the cloud fill */
+        graphics_draw_box( disp,
+            0, cloud_fill_y,
+            320, cloud_fill_h,
+            cloud_fill_color);
+
+        /* Draw the hill fill */
+        graphics_draw_box( disp,
+            0, hill_fill_y,
+            320, hill_fill_h,
+            hill_fill_color);
+
+        /* Draw the ground fill */
+        graphics_draw_box( disp,
+            0, ground_fill_y,
+            320, ground_fill_h,
+            ground_fill_color);
+
+        /* Assure RDP is ready for new commands */
+        rdp_sync( SYNC_PIPE );
+
+        /* Remove any clipping windows */
+        rdp_set_default_clipping();
+
+        /* Enable sprite display instead of solid color fill */
+        rdp_enable_texture_copy();
+
+        /* Attach RDP to display */
+        rdp_attach_display( disp );
+
+        /* Ensure the RDP is ready to receive sprites */
+        rdp_sync( SYNC_PIPE );
+
+        /* Load and draw the cloud repeat sprite */
+        rdp_load_texture( 0, 0, MIRROR_DISABLED, cloud_sprite );
+        rdp_draw_textured_rectangle( 0,
+            0, cloud_repeat_h,
+            320, cloud_repeat_h + cloud_repeat_y);
+
+        /* Ensure the RDP is ready to receive sprites */
+        rdp_sync( SYNC_PIPE );
+
+        /* Load and draw the city repeat sprite */
+        rdp_load_texture( 0, 0, MIRROR_DISABLED, city_sprite );
+        rdp_draw_textured_rectangle( 0,
+            0, city_repeat_h,
+            320, city_repeat_h + city_repeat_y);
+
+        /* Ensure the RDP is ready to receive sprites */
+        rdp_sync( SYNC_PIPE );
+
+        /* Load and draw the hill repeat sprite */
+        rdp_load_texture( 0, 0, MIRROR_DISABLED, hill_sprite );
+        rdp_draw_textured_rectangle( 0,
+            0, hill_repeat_h,
+            320, hill_repeat_h + hill_repeat_y);
+
+        /* Ensure the RDP is ready to receive sprites */
+        rdp_sync( SYNC_PIPE );
+
+        /* Load and draw the ground repeat sprite */
+        rdp_load_texture( 0, 0, MIRROR_DISABLED, ground_sprite );
+        rdp_draw_textured_rectangle( 0,
+            0, ground_repeat_h,
+            320, ground_repeat_h + ground_repeat_y);
+
+        /* Inform the RDP we are finished drawing; flush pending operations */
+        rdp_detach_display();
 
         /* Force backbuffer flip */
         display_show(disp);
-
-
     }
 }
