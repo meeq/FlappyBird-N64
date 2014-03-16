daf54c2c8306269e63e8c1473c2aa9feb9a397f3
Christopher Bonhage <me@christopherbonhage.com>
2014-03-15 23:12:22 -0700

    Tweak the sprite drawing code

--- src/main.c
+++ src/main.c
@@ -38,17 +38,17 @@ int main(void)
     s16 sky_fill_h = 141;
 
     sprite_t *cloud_sprite = read_dfs_sprite("/gfx/bg-cloud-day.sprite");
-    s16 cloud_repeat_y = 133;
+    s16 cloud_repeat_y = 119;
 
     u32 cloud_fill_color = graphics_make_color( 0xE9, 0xFC, 0xD9, 0xFF );
     s16 cloud_fill_y = 141;
     s16 cloud_fill_h = 22;
 
     sprite_t *city_sprite = read_dfs_sprite("/gfx/bg-city-day.sprite");
-    s16 city_repeat_y = 146;
+    s16 city_repeat_y = 148;
 
     sprite_t *hill_sprite = read_dfs_sprite("/gfx/bg-hill-day.sprite");
-    s16 hill_repeat_y = 160;
+    s16 hill_repeat_y = 158;
 
     u32 hill_fill_color = graphics_make_color( 0x5E, 0xE2, 0x70, 0xFF );
     s16 hill_fill_y = 169;
@@ -85,31 +85,44 @@ int main(void)
 
         /* Draw the sky fill */
         rdp_set_primitive_color( sky_fill_color );
-        rdp_draw_filled_rectangle( 0, sky_fill_y, 320, sky_fill_y + sky_fill_h );
+        rdp_draw_filled_rectangle( 0,
+            sky_fill_y, 320, sky_fill_y + sky_fill_h );
 
         /* Draw the cloud fill */
         rdp_set_primitive_color( cloud_fill_color );
-        rdp_draw_filled_rectangle( 0, cloud_fill_y, 320, cloud_fill_y + cloud_fill_h );
+        rdp_draw_filled_rectangle( 0,
+            cloud_fill_y, 320, cloud_fill_y + cloud_fill_h );
 
         /* Draw the hill fill */
         rdp_set_primitive_color( hill_fill_color );
-        rdp_draw_filled_rectangle( 0, hill_fill_y, 320, hill_fill_y + hill_fill_h );
+        rdp_draw_filled_rectangle( 0,
+            hill_fill_y, 320, hill_fill_y + hill_fill_h );
 
         /* Draw the ground fill */
         rdp_set_primitive_color( ground_fill_color );
-        rdp_draw_filled_rectangle( 0, ground_fill_y, 320, ground_fill_y + ground_fill_h );
+        rdp_draw_filled_rectangle( 0,
+            ground_fill_y, 320, ground_fill_y + ground_fill_h );
 
         /* Enable sprite display instead of solid color fill */
         rdp_enable_texture_copy();
 
-        /* Ensure the RDP is ready to receive sprites */
-        rdp_sync( SYNC_PIPE );
-
         /* Draw the cloud repeat sprite */
-        rdp_load_texture( 0, 0, MIRROR_DISABLED, cloud_sprite );
-        rdp_draw_textured_rectangle( 0,
-            0, cloud_repeat_y,
-            320, cloud_repeat_y + cloud_sprite->height);
+        for (int repeat_x = 0,
+                 slices = cloud_sprite->hslices,
+                 repeat_w = cloud_sprite->width / slices,
+                 slice;
+             repeat_x < 320; )
+        {
+            for (slice = 0;
+                 slice < slices && repeat_x < 320;
+                 slice++, repeat_x += repeat_w)
+            {
+                rdp_sync( SYNC_PIPE );
+                rdp_load_texture_stride( 0, 0, MIRROR_DISABLED,
+                    cloud_sprite, slice );
+                rdp_draw_sprite( 0, repeat_x, cloud_repeat_y );
+            }
+        }
 
         /* Ensure the RDP is ready to receive sprites */
         rdp_sync( SYNC_PIPE );
@@ -118,16 +131,28 @@ int main(void)
         rdp_load_texture( 0, 0, MIRROR_DISABLED, city_sprite );
         rdp_draw_textured_rectangle( 0,
             0, city_repeat_y,
-            320, city_repeat_y + city_sprite->height);
+            320, city_repeat_y + city_sprite->height - 1);
 
         /* Ensure the RDP is ready to receive sprites */
         rdp_sync( SYNC_PIPE );
 
         /* Draw the hill repeat sprite */
-        rdp_load_texture( 0, 0, MIRROR_DISABLED, hill_sprite );
-        rdp_draw_textured_rectangle( 0,
-            0, hill_repeat_y,
-            320, hill_repeat_y + hill_sprite->height);
+        for (int repeat_x = 0,
+                 slices = hill_sprite->hslices,
+                 repeat_w = hill_sprite->width / slices,
+                 slice;
+             repeat_x < 320; )
+        {
+            for (slice = 0;
+                 slice < slices && repeat_x < 320;
+                 slice++, repeat_x += repeat_w)
+            {
+                rdp_sync( SYNC_PIPE );
+                rdp_load_texture_stride( 0, 0, MIRROR_DISABLED,
+                    hill_sprite, slice );
+                rdp_draw_sprite( 0, repeat_x, hill_repeat_y );
+            }
+        }
 
         /* Ensure the RDP is ready to receive sprites */
         rdp_sync( SYNC_PIPE );
@@ -136,7 +161,7 @@ int main(void)
         rdp_load_texture( 0, 0, MIRROR_DISABLED, ground_sprite );
         rdp_draw_textured_rectangle( 0,
             0, ground_repeat_y,
-            320, ground_repeat_y + ground_sprite->height);
+            320, ground_repeat_y + ground_sprite->height - 1);
 
         /* Inform the RDP drawing is finished; flush pending operations */
         rdp_detach_display();
