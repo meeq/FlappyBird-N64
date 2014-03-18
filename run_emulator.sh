2a8a4ec5c89406d7cc276266fae311b45f0dfd6a
Christopher Bonhage <me@christopherbonhage.com>
2014-03-17 18:47:32 -0700

    Add skipgameinfo to MESS flags

--- run_emulator.sh
+++ run_emulator.sh
@@ -5,7 +5,7 @@
 CART_FILE="$1"
 MESS_DIR="../mess0152-64bit"
 MESS_EMU="mess64"
-MESS_FLAGS="-window -resolution 640x480"
+MESS_FLAGS="-window -resolution 640x480 -skip_gameinfo"
 
 command -v ${MESS_DIR}/${MESS_EMU} >/dev/null 2>&1 || { \
   echo >&2 'This script requires the mess command-line emulator.'; \
